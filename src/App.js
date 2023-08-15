1a5727eabfe83598299f6ab1c1fb34c3fedaa122
unknown <aprajput331998@gmail.com>
2023-08-15 23:01:04 +0530

    Components topic completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import abc from './logo.svg';
 import './App.css';
+import Home from './Components/Home'
+
+// function Home() {
+//   return (
+//     <>
+//       <div>
+//         <h1>This is Home Page</h1>
+//         <h1>And I will call this component</h1>
+//       </div>
+//       <div></div>
+//       <div></div>
+//       <div></div>
+//     </>
+//   )
+// }
 
 function App() {
 
   const arr = ["Akash", "Vikas", "NandKishor"]
+
+  const randNo = [20, 52, 85, 45, 32, 25, 55, 65]
 
   const myStyle = {
     color: 'green',
     fontSize: '80px'
   }
 
+  const details = [
+    {
+      fname: "Harshal",
+      age: 23
+    },
+    {
+      fname: "Akash",
+      age: 25
+    },
+    {
+      fname: "Piyush",
+      age: 22
+    }
+  ]
+
   return (
     <>
       <div className="App">
-        <header className="App-header">
+        <Home/>
+        {/* <header className="App-header">
           <img src={abc} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -27,11 +60,31 @@ function App() {
           >
             We are learning React
           </a>
-        </header>
+        </header> */}
+        {arr.map(a => {
+          return (
+            <h1 className='text-danger'>{a}</h1>
+          )
+        })}
+        {randNo.filter(c => c < 50).map(no => {
+          return (
+            <h2>Numbers less than 50 : {no}</h2>
+          )
+        })}
+        {
+          details.map(detail => {
+            return(
+              <div>
+                <h4>Name: {detail.fname}</h4>
+                <h4>Age: {detail.age}</h4>
+              </div>
+            )
+          })
+        }
       </div>
-      <h1></h1>
     </>
   );
 }
 
 export default App;
+// export {Home}
---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+
+function Home(){
+    return(
+        <>
+        <h1>This is Home Page</h1>
+        <h1>This is my First Component</h1>
+        </>
+    )
+}
+
+export default Home
